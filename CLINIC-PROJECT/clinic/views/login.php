830f441e78620ea2a70c95d2e873c67a9ad43b65
mo7ammed-3bdelsattar <mohammedabdelsattar110@gmail.com>
2024-09-16 19:50:46 +0200

    nav

--- CLINIC-PROJECT/clinic/views/login.php
+++ CLINIC-PROJECT/clinic/views/login.php
@@ -1,34 +1,14 @@
 <?php
  require_once "../classes/Header.php";
-
+ require_once "../classes/nav.php";
+ $nav = new Nav();
  $header=new Header();
  echo $header->getHeader('Login');
 
 ?>
 
     <div class="page-wrapper">
-        <nav class="navbar navbar-expand-lg navbar-expand-md bg-blue sticky-top">
-            <div class="container">
-                <div class="navbar-brand">
-                    <a class="fw-bold text-white m-0 text-decoration-none h3" href="./index.php">VCare</a>
-                </div>
-                <button class="navbar-toggler btn-outline-light border-0 shadow-none" type="button"
-                    data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <div class="d-flex gap-3 flex-wrap justify-content-center" role="group">
-                        <a type="button" class="btn btn-outline-light navigation--button" href="./index.php">Home</a>
-                        <a type="button" class="btn btn-outline-light navigation--button"
-                            href="./majors.php">majors</a>
-                        <a type="button" class="btn btn-outline-light navigation--button"
-                            href="./doctors/index.php">Doctors</a>
-                        <a type="button" class="btn btn-outline-light navigation--button" href="./login.php">login</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
+    <?php echo $nav->getNav(); ?>
         <div class="container">
             <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="fw-bold my-4 h4">
                 <ol class="breadcrumb justify-content-center">
--- CLINIC-PROJECT/clinic/views/majors.php
+++ CLINIC-PROJECT/clinic/views/majors.php
@@ -1,34 +1,14 @@
 <?php
  require_once "../classes/Header.php";
-
+ require_once "../classes/nav.php";
+ $nav = new Nav();
  $header=new Header();
  echo $header->getHeader('Majors');
 
 ?>
 
     <div class="page-wrapper">
-        <nav class="navbar navbar-expand-lg navbar-expand-md bg-blue sticky-top">
-            <div class="container">
-                <div class="navbar-brand">
-                    <a class="fw-bold text-white m-0 text-decoration-none h3" href="./index.php">VCare</a>
-                </div>
-                <button class="navbar-toggler btn-outline-light border-0 shadow-none" type="button"
-                    data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-                    <div class="d-flex gap-3 flex-wrap justify-content-center" role="group">
-                        <a type="button" class="btn btn-outline-light navigation--button" href="./index.php">Home</a>
-                        <a type="button" class="btn btn-outline-light navigation--button"
-                            href="./majors.php">majors</a>
-                        <a type="button" class="btn btn-outline-light navigation--button"
-                            href="./doctors/index.php">Doctors</a>
-                        <a type="button" class="btn btn-outline-light navigation--button" href="./login.php">login</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
+    <?php echo $nav->getNav(); ?>
         <div class="container">
             <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb" class="fw-bold my-4 h4">
                 <ol class="breadcrumb justify-content-center">
@@ -38,7 +18,7 @@
             </nav>
             <div class="majors-grid">
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -46,7 +26,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -54,7 +34,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -62,7 +42,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -70,7 +50,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -78,7 +58,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -86,7 +66,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -94,7 +74,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -102,7 +82,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -110,7 +90,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -118,7 +98,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
@@ -126,7 +106,7 @@
                     </div>
                 </div>
                 <div class="card p-2" style="width: 18rem;">
-                    <img src="assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
+                    <img src="../assets/images/major.jpg" class="card-img-top rounded-circle card-image-circle"
                         alt="major">
                     <div class="card-body d-flex flex-column gap-1 justify-content-center">
                         <h4 class="card-title fw-bold text-center">Major title</h4>
