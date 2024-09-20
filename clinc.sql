583f5a786f8880cdabb4def299d824ef16b28dd9
mo7ammed-3bdelsattar <mohammedabdelsattar110@gmail.com>
2024-09-20 15:17:19 +0200

    database

--- clinc.sql
+++ clinc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 18, 2024 at 01:19 PM
+-- Generation Time: Sep 20, 2024 at 03:13 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -45,12 +45,12 @@ CREATE TABLE `doctors` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `major_id` int(11) NOT NULL,
-  `start` int(11) NOT NULL,
-  `end` int(11) NOT NULL,
+  `start-end` varchar(5) NOT NULL,
   `price` int(11) NOT NULL,
   `image` varchar(300) NOT NULL,
   `phone` varchar(11) NOT NULL,
   `email` varchar(100) NOT NULL,
+  `adress` varchar(100) NOT NULL DEFAULT 'Egypt/cairo',
   `visitors` int(11) NOT NULL,
   `days` varchar(100) NOT NULL,
   `user_id` int(11) NOT NULL
@@ -60,15 +60,15 @@ CREATE TABLE `doctors` (
 -- Dumping data for table `doctors`
 --
 
-INSERT INTO `doctors` (`id`, `name`, `major_id`, `start`, `end`, `price`, `image`, `phone`, `email`, `visitors`, `days`, `user_id`) VALUES
-(17, 'Sara', 1, 10, 2, 100, 'assets/images/doctors/01.jpg', '01234567890', 'sara@gmail.com', 0, 'sun,wed', 1),
-(18, 'Nermeen', 2, 10, 2, 100, 'assets/images/doctors/02.jpg', '01234567891', 'nermeen@gmail.com', 0, 'sat,tue', 1),
-(19, 'maya', 3, 10, 2, 100, 'assets/images/doctors/03.jpg', '01234567892', 'maya@gmail.com', 0, 'mon,th', 1),
-(20, 'Marawan', 4, 2, 6, 200, 'assets/images/doctors/04.jpg', '01234567893', 'marawan@gmail.com', 0, 'sun,wed', 1),
-(21, 'Mohammed Khan', 5, 2, 6, 200, 'assets/images/doctors/05.jpg', '01234567894', 'mohammedkhan@gmail.com', 0, 'fri,tue', 1),
-(22, 'Sayed', 6, 3, 7, 200, 'assets/images/doctors/06.jpg', '01234567895', 'sayed@gmail.com', 0, 'mon,tha', 1),
-(23, 'Saad', 7, 3, 8, 200, 'assets/images/doctors/07.jpg', '01234567896', 'saad@gmail.com', 0, 'sun,wed', 1),
-(24, 'Negga', 8, 4, 4, 100, 'assets/images/doctors/08.jpg', '01234567890', 'negga@gmail.com', 0, 'all days', 1);
+INSERT INTO `doctors` (`id`, `name`, `major_id`, `start-end`, `price`, `image`, `phone`, `email`, `adress`, `visitors`, `days`, `user_id`) VALUES
+(17, 'Sara', 1, '10-2', 100, 'assets/images/doctors/01.jpg', '01234567890', 'sara@gmail.com', 'Egypt/cairo/maadi/horryah/21-4-8', 0, 'sun,wed', 1),
+(18, 'Nermeen', 2, '10-2', 100, 'assets/images/doctors/02.jpg', '01234567891', 'nermeen@gmail.com', 'Egypt/cairo/maadi/andrya/3-4-8', 0, 'sat,tue', 1),
+(19, 'maya', 3, '10-2', 100, 'assets/images/doctors/03.jpg', '01234567892', 'maya@gmail.com', 'Egypt/cairo/maadi/horryah/21-4-8', 0, 'mon,th', 1),
+(20, 'Marawan', 4, '2-6', 200, 'assets/images/doctors/04.jpg', '01234567893', 'marawan@gmail.com', 'Egypt/cairo/maadi/horryah/21-4-8', 0, 'sun,wed', 1),
+(21, 'Mohammed Khan', 5, '2-6', 200, 'assets/images/doctors/05.jpg', '01234567894', 'mohammedkhan@gmail.com', 'Egypt/cairo/maadi/horryah/21-4-8', 0, 'fri,tue', 1),
+(22, 'Sayed', 6, '3-7', 200, 'assets/images/doctors/06.jpg', '01234567895', 'sayed@gmail.com', 'Egypt/cairo/maadi/horryah/21-4-8', 0, 'mon,tha', 2),
+(23, 'Saad', 7, '3-8', 200, 'assets/images/doctors/07.jpg', '01234567896', 'saad@gmail.com', 'Egypt/cairo/maadi/andrya/3-4-8', 0, 'sun,wed', 1),
+(24, 'Negga', 8, '4-4', 100, 'assets/images/doctors/08.jpg', '01234567890', 'negga@gmail.com', 'Egypt/cairo/maadi/andrya/3-4-8', 0, 'all days', 1);
 
 -- --------------------------------------------------------
 
@@ -129,7 +129,7 @@ CREATE TABLE `patient` (
   `name` varchar(100) NOT NULL,
   `phone` varchar(11) NOT NULL,
   `email` varchar(200) NOT NULL,
-  `doctoe_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `date` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -146,6 +146,7 @@ CREATE TABLE `users` (
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   `phone` varchar(11) NOT NULL,
+  `image` varchar(100) NOT NULL DEFAULT 'assets/images/users/00.png',
   `type` enum('user','doctor','admin','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -153,8 +154,29 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `type`) VALUES
-(1, 'null', 'null', 'null', '01010101010', 'user');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `image`, `type`) VALUES
+(1, 'null', 'null', 'null', '01010101010', 'assets/images/00.png', 'user'),
+(2, 'Mohammed Abdelsattar', 'mohammedabdelsattar2@gmail.com', 'e2847a61b93f410e6f591bb546b4fe3dc7014a55', '01015616884', 'assets/images/users/00.png', 'doctor'),
+(3, 'Mohammed', 'mohammedabdelsattar@gmail.com', 'e2847a61b93f410e6f591bb546b4fe3dc7014a55', '01015616883', 'assets/images/users/00.png', 'user');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitors`
+--
+
+CREATE TABLE `visitors` (
+  `id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `visitors`
+--
+
+INSERT INTO `visitors` (`id`, `doctor_id`, `user_id`) VALUES
+(1, 22, 3);
 
 --
 -- Indexes for dumped tables
@@ -192,13 +214,21 @@ ALTER TABLE `majors`
 ALTER TABLE `patient`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `doctor_id` (`doctoe_id`);
+  ADD KEY `doctor_id` (`doctor_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `visitors`
+--
+ALTER TABLE `visitors`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `doctor_id` (`doctor_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -232,12 +262,18 @@ ALTER TABLE `majors`
 -- AUTO_INCREMENT for table `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `visitors`
+--
+ALTER TABLE `visitors`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
@@ -255,8 +291,15 @@ ALTER TABLE `doctors`
 -- Constraints for table `patient`
 --
 ALTER TABLE `patient`
-  ADD CONSTRAINT `doctor_id` FOREIGN KEY (`doctoe_id`) REFERENCES `doctors` (`id`),
+  ADD CONSTRAINT `doctor_id` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`),
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Constraints for table `visitors`
+--
+ALTER TABLE `visitors`
+  ADD CONSTRAINT `visitors_ibfk_1` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`),
+  ADD CONSTRAINT `visitors_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
