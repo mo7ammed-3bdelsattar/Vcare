2506f0412f717ccb33ea6ee453a6c0c97acd7b07
mo7ammed-3bdelsattar <mohammedabdelsattar110@gmail.com>
2024-09-20 15:26:41 +0200

    database

--- clinc.sql
+++ clinc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 20, 2024 at 03:13 PM
+-- Generation Time: Sep 20, 2024 at 03:25 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
